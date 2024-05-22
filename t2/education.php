77bdf67f116a69f30ff75062edae298aa23d98d5
jape03 <japol33333@gmail.com>
2024-05-23 02:58:26 +0800

    t2

---
+++ t2/education.php
@@ -0,0 +1,9 @@
+<?php
+$education_degree = "Bachelor of Science in Computer Science with Specialization in Software Engineering";
+$education_institution = "FEU Institute of Technology, Manila";
+?>
+
+<div>
+    <span class="degree"><?php echo $education_degree; ?></span>
+</div>
+<div class="institution"><?php echo $education_institution; ?></div>
